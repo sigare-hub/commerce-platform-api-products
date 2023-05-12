f80fff9cd0df56246e8abcd6a8958179a1437162
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-05-12 17:16:09 -0300

    wip: add product review model

---
+++ src/main/java/com/commerceplatform/api/products/models/ProductReviewModel.java
@@ -0,0 +1,29 @@
+package com.commerceplatform.api.products.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product_review")
+public class ProductReviewModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private ProductModel product;
+
+    private Long customerId;
+    private String comment;
+    private boolean published;
+}
